47f589555c7aab3a485f0270c36ae2f094708137
Marcio Ramos <cubomarrcio@gmail.com>
2015-02-21 21:39:13 +0900

    minor changes

--- resources/util/kanji.py
+++ resources/util/kanji.py
@@ -1,16 +1,15 @@
 class Kanji:
     to_db = {'_id': '_id', 'kanji': 'k', 'looks_like': 'll', 'meaning_like': 'ml',
               'contains': 'c', 'grade': 'g', 'is_contained': 'ic', 'master_relation': 'mr',
-              'frequency': 'f', 'num_strokes': 'ns'}
+              'num_strokes': 'ns'}
     to_obj = {v:k for k,v in to_db.items()}
 
     def __init__(self, _id, kanji, looks_like=None, meaning_like=None, contains=None, grade='0',
-                 is_contained=None, master_relation='', frequency=0, num_strokes=0):
+                 is_contained=None, master_relation='', num_strokes=0):
         self._id = _id
         self.kanji = kanji
         self.grade = grade
         self.master_relation = master_relation
-        self.frequency = frequency
         self.num_strokes = num_strokes
         self.meaning_like = meaning_like if meaning_like is not None else []
         self.is_contained = is_contained if is_contained is not None else []
@@ -19,3 +18,9 @@ class Kanji:
 
     def minify_keys(self):
         return {Kanji.to_db[k]:v for k,v in vars(self).copy().items()}
+
+    def __str__(self):
+        ans = u""
+        for k,v in vars(self).items():
+            ans += unicode(k) + u": " + unicode(v) + u"\n"
+        return ans
