19185235d98301020b9b18279593b4a3ad28724f
Marcio V. Ramos <cubomarrcio@gmail.com>
2015-02-21 11:02:54 +0900

    skeleton and database

---
+++ resources/util/kanji.py
@@ -0,0 +1,4 @@
+class Kanji:
+    def __init__(self, id, kanji, looks_like=None, meaning_like=None, contains=None,
+                 is_contained=None, master_relation=None, frequency=None, num_strokes=None):
+        pass
