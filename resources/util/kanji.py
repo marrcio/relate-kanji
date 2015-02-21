2cc87c8d8d2161a227bc20264541e65f538f755d
Marcio V. Ramos <cubomarrcio@gmail.com>
2015-02-21 18:51:02 +0900

    Changing structure

--- resources/util/kanji.py
+++ resources/util/kanji.py
@@ -1,4 +1,13 @@
 class Kanji:
-    def __init__(self, id, kanji, looks_like=None, meaning_like=None, contains=None,
-                 is_contained=None, master_relation=None, frequency=None, num_strokes=None):
-        pass
+    def __init__(self, _id, kanji, looks_like=None, meaning_like=None, contains=None, grade='0',
+                 is_contained=None, master_relation='', frequency=0, num_strokes=0):
+        self._id = _id
+        self.kanji = kanji
+        self.grade = grade
+        self.master_relation = master_relation
+        self.frequency = frequency
+        self.num_strokes = num_strokes
+        self.meaning_like = meaning_like if meaning_like is not None else []
+        self.is_contained = is_contained if is_contained is not None else []
+        self.contains = contains if contains is not None else []
+        self.looks_like = looks_like if looks_like is not None else []
